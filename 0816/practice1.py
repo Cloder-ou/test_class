c213711db71254c9908b32b76d2e52cb133eadf2
cloder-ou <atclouding@gmail.com>
2026-08-16 11:41:21 +0800

    0816截止上午課程

---
+++ 0816/practice1.py
@@ -0,0 +1,12 @@
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client()
+
+interaction = client.interactions.create(
+    model="gemini-3.6-flash",
+    input="列出超級瑪莉所有的版本名稱"
+)
+print(interaction.output_text)
